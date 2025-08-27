a9975d76e0150cf26608ce89cc5708e512cbf8de
PaulMick <paulwmick@gmail.com>
2025-08-27 18:16:57 -0400

    added template secrets

---
+++ main/secrets.c
@@ -0,0 +1,4 @@
+char *wifi_ssid = "";
+char *wifi_pass = "";
+
+char *weather_api_key = "";
---
+++ main/secrets.h
@@ -0,0 +1,9 @@
+#ifndef SECRETS_H
+#define SECRETS_H
+
+extern char* wifi_ssid;
+extern char* wifi_pass;
+
+extern char* weather_api_key;
+
+#endif
