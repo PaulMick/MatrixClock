feb6d33cdcb3a0e7c4153d6540acf1d4591c6888
PaulMick <paulwmick@gmail.com>
2025-08-31 18:31:18 -0400

    maybe making progress

--- main/display_driver.c
+++ main/display_driver.c
@@ -3,7 +3,6 @@
 #include <string.h>
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
-#include "driver/i2s_std.h"
 #include "driver/gpio.h"
 #include "esp_rom_sys.h"
 #include "esp_heap_caps.h"
@@ -43,17 +42,13 @@ uint8_t (*frame_buf_in_ptr)[DISPLAY_HEIGHT][DISPLAY_WIDTH][3] = &frame_buf_in;
 uint8_t (*frame_buf_out_ptr)[DISPLAY_HEIGHT][DISPLAY_WIDTH][3] = &frame_buf_out;
 // Temp pointer for swapping frame buffer pointers
 uint8_t (*tmp)[DISPLAY_HEIGHT][DISPLAY_WIDTH][3];
-int swap = 0;
 
 // Indicator of whether the in frame buffer is completed
 int in_done = 0;
 int *in_done_ptr = &in_done;
 
 // Scanning bitplanes used to correctly format row pixel data into the correct format for RGB channel PWM
-uint8_t *bitplane_buf[COLOR_DEPTH];
-
-// I2S channel handle
-i2s_chan_handle_t tx_chan;
+uint8_t bitplane_buf[DISPLAY_HEIGHT][DISPLAY_WIDTH][COLOR_DEPTH];
 
 // Get a display handle
 DisplayHandle get_display_handle() {
@@ -66,48 +61,6 @@ DisplayHandle get_display_handle() {
     return display_handle;
 }
 
-void init_gpio() {
-    gpio_config_t gpio_cfg = {
-        .pin_bit_mask = ((1ULL << A) | (1ULL << B) | (1ULL << C) | (1ULL << D) | (1ULL << LAT) | (1ULL << OE) | (1ULL << CLK)),
-        .mode = GPIO_MODE_OUTPUT,
-        .pull_up_en = 0,
-        .pull_down_en = 0,
-        .intr_type = GPIO_INTR_DISABLE
-    };
-    gpio_config(&gpio_cfg);
-    gpio_set_level(LAT, 0);
-    gpio_set_level(OE, 1);
-}
-
-void init_i2s() {
-    i2s_chan_config_t chan_cfg = I2S_CHANNEL_DEFAULT_CONFIG(I2S_NUM_0, I2S_ROLE_MASTER);
-    chan_cfg.dma_desc_num = 64;
-    chan_cfg.dma_frame_num = 64;
-    chan_cfg.auto_clear = true;
-    i2s_new_channel(&chan_cfg, &tx_chan, NULL);
-    i2s_std_config_t std_cfg = {
-        .clk_cfg = I2S_STD_CLK_DEFAULT_CONFIG(I2S_SAMPLE_RATE),
-        .slot_cfg = I2S_STD_PHILIPS_SLOT_DEFAULT_CONFIG(I2S_DATA_BIT_WIDTH_8BIT, I2S_SLOT_MODE_MONO),
-        .gpio_cfg = {
-            .mclk = I2S_GPIO_UNUSED,
-            .bclk = CLK,
-            .ws = I2S_GPIO_UNUSED,
-            .dout = {R1, G1, B1, R2, G2, B2, -1, -1},
-            .din = I2S_GPIO_UNUSED
-        }
-    };
-    i2s_channel_init_std_mode(tx_chan, &std_cfg);
-    i2s_channel_enable(tx_chan);
-}
-
-// TODO: Might not be needed???
-void set_row(uint8_t row) {
-    gpio_set_level(A, row & 0x01);
-    gpio_set_level(B, row & 0x02);
-    gpio_set_level(C, row & 0x04);
-    gpio_set_level(D, row & 0x08);
-}
-
 // Swaps the pointers to the two frame buffers
 void swap_frame_buffers() {
     tmp = frame_buf_in_ptr;
@@ -116,42 +69,49 @@ void swap_frame_buffers() {
 }
 
 // Prepare the bitplanes for a pair of rows
-void prep_bitplanes(uint8_t row) {
-    // Add color channels in by bit depth
-    for (int bit_depth = 0; bit_depth < COLOR_DEPTH; bit_depth ++) {
-        // Add the bit at the current bit depth from every pixel in the row to the bitplane
+void prep_bitplanes() {
+    for (uint8_t row = 0; row < (uint8_t) (DISPLAY_HEIGHT / SCAN_LINES); row ++) {
         for (int col = 0; col < DISPLAY_WIDTH; col ++) {
-            uint8_t r1 = *frame_buf_out_ptr[row][col][0];
-            uint8_t g1 = *frame_buf_out_ptr[row][col][1];
-            uint8_t b1 = *frame_buf_out_ptr[row][col][2];
-            uint8_t r2 = *frame_buf_out_ptr[row + DISPLAY_HEIGHT / SCAN_LINES][col][0];
-            uint8_t g2 = *frame_buf_out_ptr[row + DISPLAY_HEIGHT / SCAN_LINES][col][1];
-            uint8_t b2 = *frame_buf_out_ptr[row + DISPLAY_HEIGHT / SCAN_LINES][col][2];
-            uint8_t rgb_bit_slice = 0;
-            rgb_bit_slice |= ((r1 >> (7 - bit_depth)) & 1) << 0;
-            rgb_bit_slice |= ((g1 >> (7 - bit_depth)) & 1) << 1;
-            rgb_bit_slice |= ((b1 >> (7 - bit_depth)) & 1) << 2;
-            rgb_bit_slice |= ((r2 >> (7 - bit_depth)) & 1) << 3;
-            rgb_bit_slice |= ((g2 >> (7 - bit_depth)) & 1) << 4;
-            rgb_bit_slice |= ((b2 >> (7 - bit_depth)) & 1) << 5;
-            bitplane_buf[bit_depth][col] = rgb_bit_slice;
+            for (int depth = 0; depth < COLOR_DEPTH; depth ++) {
+                uint8_t r1 = (*frame_buf_out_ptr)[row][col][0];
+                uint8_t g1 = (*frame_buf_out_ptr)[row][col][1];
+                uint8_t b1 = (*frame_buf_out_ptr)[row][col][2];
+                uint8_t r2 = (*frame_buf_out_ptr)[row + DISPLAY_HEIGHT / SCAN_LINES][col][0];
+                uint8_t g2 = (*frame_buf_out_ptr)[row + DISPLAY_HEIGHT / SCAN_LINES][col][1];
+                uint8_t b2 = (*frame_buf_out_ptr)[row + DISPLAY_HEIGHT / SCAN_LINES][col][2];
+                printf("r1 %d, g1 %d, b1 %d, r2 %d, g2 %d, b2 %d ", r1, g1, b2, r2, g2, b2);
+                uint8_t rgb_bit_slice = 0;
+                rgb_bit_slice |= ((r1 >> (7 - depth)) & 1) << 7;
+                rgb_bit_slice |= ((g1 >> (7 - depth)) & 1) << 6;
+                rgb_bit_slice |= ((b1 >> (7 - depth)) & 1) << 5;
+                rgb_bit_slice |= ((r2 >> (7 - depth)) & 1) << 4;
+                rgb_bit_slice |= ((g2 >> (7 - depth)) & 1) << 3;
+                rgb_bit_slice |= ((b2 >> (7 - depth)) & 1) << 2;
+                printf("slice %d\n", rgb_bit_slice);
+                bitplane_buf[row][col][depth] = rgb_bit_slice;
+            }
         }
     }
 }
 
-// Render a row to the display
-void render_row(uint8_t row) {
-    set_row(row);
-    prep_bitplanes(row);
-    for (int bit_depth = 0; bit_depth < COLOR_DEPTH; bit_depth ++) {
-        size_t bytes_written;
-        gpio_set_level(OE, 1);
-        i2s_channel_write(tx_chan, bitplane_buf[bit_depth], DISPLAY_WIDTH, &bytes_written, 1);
-        gpio_set_level(LAT, 1);
-        gpio_set_level(LAT, 0);
-        gpio_set_level(OE, 0);
-        esp_rom_delay_us(BASE_DELAY_US * (1 << bit_depth));
-    }
+void init_gpio() {
+    gpio_config_t gpio_cfg = {
+        .pin_bit_mask = ((1ULL << R1) | (1ULL << G1) | (1ULL << B1) | (1ULL << R2) | (1ULL << G2) | (1ULL << B2) | (1ULL << A) | (1ULL << B) | (1ULL << C) | (1ULL << D) | (1ULL << LAT) | (1ULL << OE) | (1ULL << CLK)),
+        .mode = GPIO_MODE_OUTPUT,
+        .pull_up_en = 0,
+        .pull_down_en = 0,
+        .intr_type = GPIO_INTR_DISABLE
+    };
+    gpio_config(&gpio_cfg);
+    gpio_set_level(CLK, 0);
+    gpio_set_level(LAT, 0);
+    gpio_set_level(OE, 0);
+    gpio_set_level(R1, 0);
+    gpio_set_level(G1, 0);
+    gpio_set_level(B1, 0);
+    gpio_set_level(R2, 0);
+    gpio_set_level(G2, 0);
+    gpio_set_level(B2, 0);
 }
 
 void refresh_task(void *param) {
@@ -160,9 +120,40 @@ void refresh_task(void *param) {
             swap_frame_buffers();
             *in_done_ptr = 0;
         }
-        for (uint8_t row = 0; row < (uint8_t) (DISPLAY_HEIGHT / SCAN_LINES); row ++) {
-            render_row(row);
+        // Main render
+        // printf("b %d\n", bitplane_buf[0][0][0]);
+        prep_bitplanes();
+        // printf("c %d\n", bitplane_buf[0][0][0]);
+        for (int depth = 0; depth < COLOR_DEPTH; depth ++) {
+            for (int i = 0; i < 1 << depth; i ++) {
+                for (uint8_t row = 0; row < (uint8_t) (DISPLAY_HEIGHT / SCAN_LINES); row ++) {
+                    gpio_set_level(A, (row & 0x01) >> 0);
+                    gpio_set_level(B, (row & 0x02) >> 1);
+                    gpio_set_level(C, (row & 0x04) >> 2);
+                    gpio_set_level(D, (row & 0x08) >> 3);
+                    for (uint8_t col = 0; col < DISPLAY_WIDTH; col ++) {
+                        
+                        gpio_set_level(R1, (bitplane_buf[row][col][depth] & 0x01) >> 0);
+                        gpio_set_level(G1, (bitplane_buf[row][col][depth] & 0x02) >> 1);
+                        gpio_set_level(B1, (bitplane_buf[row][col][depth] & 0x04) >> 2);
+                        gpio_set_level(R2, (bitplane_buf[row][col][depth] & 0x08) >> 3);
+                        gpio_set_level(G2, (bitplane_buf[row][col][depth] & 0x10) >> 4);
+                        gpio_set_level(B2, (bitplane_buf[row][col][depth] & 0x20) >> 5);
+                        // gpio_set_level(R1, 0);
+                        // gpio_set_level(G1, 0);
+                        // gpio_set_level(B1, 0);
+                        // gpio_set_level(R2, 0);
+                        // gpio_set_level(G2, 0);
+                        // gpio_set_level(B2, 0);
+                        gpio_set_level(CLK, 1);
+                        gpio_set_level(CLK, 0);
+                    }
+                    gpio_set_level(LAT, 1);
+                    gpio_set_level(LAT, 0);
+                }
+            }
         }
+        // End main render
         vTaskDelay(10 / portTICK_PERIOD_MS);
     }
 }
@@ -170,12 +161,21 @@ void refresh_task(void *param) {
 // Call this to start the display refresh task
 void run_refresh() {
     init_gpio();
-    init_i2s();
 
-    for (int bit_depth = 0; bit_depth < COLOR_DEPTH; bit_depth ++) {
-        bitplane_buf[bit_depth] = heap_caps_malloc(DISPLAY_WIDTH, MALLOC_CAP_DMA);
-        memset(bitplane_buf[bit_depth], 0, DISPLAY_WIDTH);
+    // for (int row = 0; row < (DISPLAY_HEIGHT / SCAN_LINES);  row ++) {
+    //     for (int bit_depth = 0; bit_depth < COLOR_DEPTH; bit_depth ++) {
+    //         bitplane_buf[row][bit_depth] = heap_caps_malloc(DISPLAY_WIDTH, MALLOC_CAP_DMA);
+    //         memset(bitplane_buf[row][bit_depth], 0, DISPLAY_WIDTH);
+    //     }
+    // }
+    for (int i = 0; i < DISPLAY_HEIGHT; i ++) {
+        for (int j = 0; j < DISPLAY_WIDTH; j ++) {
+            for (int d = 0; d < COLOR_DEPTH; d ++) {
+                bitplane_buf[i][j][d] = 255;
+            }
+        }
     }
+    // printf("a %d\n", bitplane_buf[0][0][0]);
 
     for (int y = 0; y < DISPLAY_HEIGHT; y ++) {
         for (int x = 0; x < DISPLAY_WIDTH; x ++) {
